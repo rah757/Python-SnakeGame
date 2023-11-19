f6874433e4b545f8bb4d437d29465374f28feb38
Rah <arrehman757@gmail.com>
2023-11-19 23:09:13 +0530

    added default starting snake

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+
+screen.bgcolor("black")
+
+screen.title("Snake game")
+
+turtles = []
+
+x = 0
+y = 0
+
+startPosition = [(0,0), (-20,0), (-40,0)]
+
+
+for pos in startPosition:
+    carl = Turtle(shape='square')
+    carl.color("white")
+    carl.goto(pos)
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
